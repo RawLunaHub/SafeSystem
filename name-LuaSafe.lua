ce8c7edfbe33c4f0ebb5507ea132d686ba350f79
Ckituzer <83168446+RawLunaHub@users.noreply.github.com>
2023-05-14 17:01:09 +0300

    Update name-LuaSafe.lua

--- name-LuaSafe.lua
+++ name-LuaSafe.lua
@@ -3,11 +3,10 @@ if not game:IsLoaded() then
     game.Loaded:wait()  
 end
 script.Name = "J毅"
-function check()
-    game:GetService("Players").LocalPlayer.GameplayPaused = true
-    task.wait(3.2)
-    game:GetService("Players").LocalPlayer.GameplayPaused = false
-end
+game:GetService("Players").LocalPlayer.GameplayPaused = true
+task.wait(3.2)
+game:GetService("Players").LocalPlayer.GameplayPaused = false
+
 --[[
 local eYfJfkdaff = loadstring
 local getSHR0cEdldA = HttpGet
