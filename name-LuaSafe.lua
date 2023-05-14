ea9c09e0eb0c3d9f72398f4db2a2f9f8d3c9cb16
Ckituzer <83168446+RawLunaHub@users.noreply.github.com>
2023-05-14 16:22:24 +0300

    Create name-LuaSafe.lua
    Safe

---
+++ name-LuaSafe.lua
@@ -0,0 +1,10 @@
+randomizer = math.random(1, 100)
+script.Name = "Jq䜮*m1ZxYn" ..randomizer
+if not game:IsLoaded() then
+    game.Loaded:wait()  
+end
+game:GetService("Players").LocalPlayer.GameplayPaused = true
+game:GetService("Players").LocalPlayer.GameplayPaused = false
+game:GetService("Players").LocalPlayer.GameplayPaused = true
+task.wait(3.2)
+game:GetService("Players").LocalPlayer.GameplayPaused = true
